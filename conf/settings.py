9689d48f948847a8539232b5b1b9543170ec123b
Kuai-sama <tmsbgn@gmail.com>
2024-03-19 00:01:13 +0100

    develop: add settings.py file and EnvManager class

---
+++ conf/settings.py
@@ -0,0 +1,17 @@
+OUTPUT_FOLDER = Path(__file__).parent.parent.joinpath("output")
+
+
+class EnvManager:
+    class ExceptionEnvVarNotFound(Exception):
+        pass
+
+    def __init__(self):
+        self.env = None
+        self.config = None
+
+    def check_if_env_is_set(self, env_var) -> bool:
+        if not getenv(env_var):
+            msg = f"Can't find the environment variable `{env_var}` in the environment"
+            raise self.ExceptionEnvVarNotFound(msg)
+        return True
+
