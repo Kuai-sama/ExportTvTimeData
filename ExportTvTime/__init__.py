430e7615e1ace203391faa29919a0c507719c5e5
Kuai-sama <tmsbgn@gmail.com>
2024-03-19 23:30:03 +0100

    develop: read dotenv file when import conf folder

--- ExportTvTime/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from dotenv import load_dotenv
-
-env_folder = Path(__file__).parent.parent.joinpath("conf", "env")
-print(f"{env_folder = }")
- 
-load_dotenv(env_folder.joinpath("tvtime.env"))
